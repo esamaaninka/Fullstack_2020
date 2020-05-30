3a361da1baf4659fbf44f92322b50b08ff0e3023
esamaaninka <61664607+esamaaninka@users.noreply.github.com>
2020-05-30 10:45:50 +0300

    4.1 use MongoDB Atlas, reqeustlogger, error handler, REST get and post

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -13,11 +14,23 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+//const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
+
+// added requestlogger 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -36,6 +49,27 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId')  {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3003
 app.listen(PORT, () => {
