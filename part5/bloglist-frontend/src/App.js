87ae3182d5f826ce00fd4a54bc7a03ed4bb01131
esamaaninka <61664607+esamaaninka@users.noreply.github.com>
2020-12-04 10:39:08 +0200

    excercise 5.5 (not with Togglable)

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [blogFormVisible, setBlogFormVisible] =useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -62,33 +63,51 @@ const App = () => {
 
   
 
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <div> title:
-      <input 
-        value={newBlog.title || ''} // "...|| '')" to get rid of "Warning: A component is changing an uncontrolled input of type text to be controlled. Input elements should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled input element for the lifetime of the component.*"
-        name="title"
-        onChange={handleBlogChange}
-      />
+  const blogForm = () => {
+    const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+    const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setBlogFormVisible(true)}>New blog</button>
+        </div>
+        <div style={showWhenVisible} >
+          <form onSubmit={addBlog}>
+            <div> title:
+            <input 
+              value={newBlog.title || ''} // "...|| '')" to get rid of "Warning: A component is changing an uncontrolled input of type text to be controlled. Input elements should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled input element for the lifetime of the component.*"
+              name="title"
+              onChange={handleBlogChange}
+            />
+            </div>
+            <div> author:
+            <input
+              value={newBlog.author || ''}
+              name="author"
+              onChange={handleBlogChange}
+            />
+            </div>
+            <div> url:
+            <input
+              value={newBlog.url || ''}
+              name="url"
+              onChange={handleBlogChange}
+            />
+          </div>
+          <button onClick={() => {
+            setBlogFormVisible(false)
+            setNewBlog('') 
+            }
+          }>cancel</button>
+          <button type="submit">Create</button>
+        </form>  
+        </div>
       </div>
-      <div> author:
-       <input
-        value={newBlog.author || ''}
-        name="author"
-        onChange={handleBlogChange}
-      />
-      </div>
-      <div> url:
-       <input
-        value={newBlog.url || ''}
-        name="url"
-        onChange={handleBlogChange}
-      />
-      </div>
-      
-      <button type="submit">Create</button>
-    </form>  
-  )
+    )
+  }
+
+
   const handleBlogChange = (event) => {
     //console.log('handleblogchange updating: ', event.target.value)
     setNewBlog({
@@ -99,20 +118,22 @@ const App = () => {
 
   const addBlog = (event) => {
     event.preventDefault()
-    //console.log('addBlog called', newBlog)
-
-    const blogObject = {
-      title: newBlog.title,
-      author: newBlog.author,
-      url: newBlog.url 
+    //console.log('Was blogform cancelled', blogFormVisible, newBlog) 
+    if(blogFormVisible) { // if blogform was not cancelled (but why we enter here after cancel button ? ) 
+      const blogObject = {
+        title: newBlog.title,
+        author: newBlog.author,
+        url: newBlog.url 
+      }
+      //console.log('attempt to send blog: ', blogObject)
+      blogService.create(blogObject)
+        .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          setNewBlog('')
+        })
+    } else {
+      setNewBlog('')
     }
-    console.log('attempt to send blog: ', blogObject)
-    blogService.create(blogObject)
-      .then(returnedBlog => {
-        setBlogs(blogs.concat(returnedBlog))
-        setNewBlog('')
-      })
-    
     
   }
 
