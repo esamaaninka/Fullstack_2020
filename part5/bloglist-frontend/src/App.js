ba4d40c16183abba098e09f0488610e9b0c983e0
esamaaninka <esa.maaninka@hotmail.com>
2020-12-08 10:54:33 +0200

    Revert "temp excercise 5.6"
    This reverts commit 4cb1b7b7fed2e6e0c944b6a2043e27c5835d860a.

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,16 +3,15 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
-import blogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-  //const [blogFormVisible, setBlogFormVisible] =useState(false)
-  //const [newBlog, setNewBlog] = useState('')
+  const [blogFormVisible, setBlogFormVisible] =useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -64,7 +63,80 @@ const App = () => {
 
   
 
-  
+  const blogForm = () => {
+    const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+    const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setBlogFormVisible(true)}>New blog</button>
+        </div>
+        <div style={showWhenVisible} >
+          <form onSubmit={addBlog}>
+            <div> title:
+            <input 
+              value={newBlog.title || ''} // "...|| '')" to get rid of "Warning: A component is changing an uncontrolled input of type text to be controlled. Input elements should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled input element for the lifetime of the component.*"
+              name="title"
+              onChange={handleBlogChange}
+            />
+            </div>
+            <div> author:
+            <input
+              value={newBlog.author || ''}
+              name="author"
+              onChange={handleBlogChange}
+            />
+            </div>
+            <div> url:
+            <input
+              value={newBlog.url || ''}
+              name="url"
+              onChange={handleBlogChange}
+            />
+          </div>
+          <button onClick={() => {
+            setBlogFormVisible(false)
+            setNewBlog('') 
+            }
+          }>cancel</button>
+          <button type="submit">Create</button>
+        </form>  
+        </div>
+      </div>
+    )
+  }
+
+
+  const handleBlogChange = (event) => {
+    //console.log('handleblogchange updating: ', event.target.value)
+    setNewBlog({
+      ...newBlog,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    //console.log('Was blogform cancelled', blogFormVisible, newBlog) 
+    if(blogFormVisible) { // if blogform was not cancelled (but why we enter here after cancel button ? ) 
+      const blogObject = {
+        title: newBlog.title,
+        author: newBlog.author,
+        url: newBlog.url 
+      }
+      //console.log('attempt to send blog: ', blogObject)
+      blogService.create(blogObject)
+        .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          setNewBlog('')
+        })
+    } else {
+      setNewBlog('')
+    }
+    
+  }
+
   
   
 
