9fcf93118997232d97de4e1f3eb199eaf769b76f
esamaaninka <esa.maaninka@hotmail.com>
2021-02-07 09:55:57 +0200

    excercise 6.20 new anecdote redux- connect

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,8 +1,42 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
+//import { useDispatch } from 'react-redux'
 import { newAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification } from '../reducers/notificationReducer'
 
+// redux connect way
+const NewAnecdote = (props) => {
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    props.newAnecdote(content)
+  }
+  return (
+    <form onSubmit={addAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">Create new</button>
+    </form>
+  )
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  newAnecdote: content => {
+    dispatch(newAnecdote(content))
+    dispatch(showNotification(`You added anecdote: "${content}"`, 5))
+  }
+})
+
+
+const ConnectedNewAnecdote = connect( 
+  null,//mapStateToProps
+  mapDispatchToProps
+)(NewAnecdote)
+
+export default ConnectedNewAnecdote
+
+/* react-redux 
 const NewAnecdote = () => {
   const dispatch = useDispatch()
   
@@ -24,3 +58,4 @@ const NewAnecdote = () => {
 }
 
 export default NewAnecdote
+*/
